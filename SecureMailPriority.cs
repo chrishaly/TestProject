40e370ebe1f94d507741cb1610c86ffe87d14d5c
Chris Huang <chrishwang@live.cn>
2013-04-28 09:17:56 +0800

    first commit

---
+++ SecureMailPriority.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Cpi.Net.SecureMail
+{
+    /// <summary>
+    /// Specifies the priority of a SecureMailMessage.
+    /// </summary>
+    public enum SecureMailPriority
+    {
+        /// <summary>
+        /// The e-mail has normal priority.
+        /// </summary>
+        Normal,
+        /// <summary>
+        /// The e-mail has low priority.
+        /// </summary>
+        Low,
+        /// <summary>
+        /// The e-mail has high priority.
+        /// </summary>
+        High
+    }
+}
